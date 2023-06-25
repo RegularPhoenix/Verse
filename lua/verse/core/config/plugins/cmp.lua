e10825124c79c9ec0c90c8811147552eb3ffab6a
RegularPhoenix <artiom.phoenix@gmail.com>
2023-06-25 12:20:44 +0300

    Add borders to cmp windows

--- lua/verse/core/config/plugins/cmp.lua
+++ lua/verse/core/config/plugins/cmp.lua
@@ -5,24 +5,29 @@ function M.load()
 
 	cmp.setup({
 		 mapping = cmp.mapping.preset.insert({
-			['<C-b>'] = cmp.mapping.scroll_docs(-4),
-			['<C-f>'] = cmp.mapping.scroll_docs(4),
-			['<C-Space>'] = cmp.mapping.complete(),
-			['<C-e>'] = cmp.mapping.abort(),
-			['<CR>'] = cmp.mapping.confirm({ select = true }),
+			["<C-b>"] = cmp.mapping.scroll_docs(-4),
+			["<C-f>"] = cmp.mapping.scroll_docs(4),
+			["<C-Space>"] = cmp.mapping.complete(),
+			["<C-e>"] = cmp.mapping.abort(),
+			["<CR>"] = cmp.mapping.confirm({ select = true }),
 		}),
 
 		sources = cmp.config.sources({
-			{ name = 'nvim_lsp' },
-			{ name = 'luasnip' },
-		}, {
-			{ name = 'buffer' },
-			{ name = 'path' },
+			{ name = "nvim_lsp", priority = 4 },
+			{ name = "luasnip", priority = 3 },
+			{ name = "buffer", priority = 2 },
+			{ name = "path", priority = 1 },
 		}),
+
 		snippet = {
 			expand = function(args)
 				require("luasnip").lsp_expand(args.body)
 			end,
+		},
+
+		window = {
+			completion = cmp.config.window.bordered({ border = "single" }),
+			documentation = cmp.config.window.bordered({ border = "single" })
 		}
 	})
 end
