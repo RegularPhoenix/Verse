644ae2cf5da7c6b37ba61d2aa46c9ac39837ffe4
RegularPhoenix <artiom.phoenix@gmail.com>
2024-03-05 12:22:41 +0300

    Add floating window for LSP

--- lua/verse/config/plugins/lsp.lua
+++ lua/verse/config/plugins/lsp.lua
@@ -7,11 +7,33 @@ M.override_attach = function(client, bufnr)
 	end
 end
 
+M.border = {
+     {"🭽", "FloatBorder"},
+     {"▔", "FloatBorder"},
+     {"🭾", "FloatBorder"},
+     {"▕", "FloatBorder"},
+     {"🭿", "FloatBorder"},
+     {"▁", "FloatBorder"},
+     {"🭼", "FloatBorder"},
+     {"▏", "FloatBorder"},
+}
+
 function M.load()
+	local orig_util_open_floating_window = vim.lsp.util.open_floating_preview
+	function vim.lsp.util.open_floating_preview(content, syntax, opts, ...)
+		opts = opts or {}
+		opts.border = opts.border or M.border
+		return orig_util_open_floating_window(content, syntax, opts, ...)
+	end
+
 	require("neodev").setup()
+	local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 	for _, server in pairs(require("verse.core.util").option_or_default("language_servers" , {})) do
-		require("lspconfig")[server].setup({ on_attach = M.override_attach })
+		require("lspconfig")[server].setup({
+			on_attach = M.override_attach,
+			capabilities = capabilities
+		})
 	end
 end
 
